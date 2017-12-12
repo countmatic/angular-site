d9dfcfec522c197255b58bc496885f0ab95b4671
Rainer Feike <rainer.feike@fiduciagad.de>
2017-12-12 15:08:34 +0100

    localStore rein

--- src/app/token-input-form/token-input-form.component.ts
+++ src/app/token-input-form/token-input-form.component.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {ActivatedRoute, ParamMap} from "@angular/router";
 
 @Component({
   selector: 'app-token-input-form',
@@ -7,23 +8,48 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 })
 export class TokenInputFormComponent implements OnInit {
 
-
-  submitted = false;
-  token = '';
-  store = false;
+  private token = '';
+  private store = false;
+  private oldTokens : string[];
 
   @Output() onTokenChanged = new EventEmitter<string>();
 
-  constructor() {
+  constructor(private activatedRoute: ActivatedRoute) {
+    console.log("Hello TokenInputFormComponent constructor");
   }
 
   ngOnInit() {
-    //localStorage.getItem("cm-tokens");
+    console.log("Hello TokenInputFormComponent ngOnInit");
+    let oldTokenStore = localStorage.getItem('oldTokens');
+    if (null == oldTokenStore) {
+      oldTokenStore = '[]';
+    }
+    this.oldTokens = JSON.parse(oldTokenStore);
+    console.log("TokenInputFormComponent oldstuff");
+    console.log(this.oldTokens);
 
+    let urlToken = null;
+    this.activatedRoute.queryParamMap.subscribe((next: ParamMap) => {
+      urlToken = next.get("token")
+      if (urlToken) {
+        console.log(urlToken);
+        this.token = urlToken;
+      } else {
+        console.log("no token given in url");
+      }
+    });
   }
 
   onSubmit() {
-    this.submitted = true;
+    console.log("Hello TokenInputFormComponent onSubmit");
+    if (this.store) {
+      console.log("TokenInputFormComponent storing input");
+      let n = this.oldTokens.unshift(this.token);
+      if (n > 10) {
+        this.oldTokens = this.oldTokens.slice(0, 9);
+      }
+      localStorage.setItem('oldTokens', JSON.stringify(this.oldTokens));
+    }
     this.onTokenChanged.emit(this.token);
   }
 
